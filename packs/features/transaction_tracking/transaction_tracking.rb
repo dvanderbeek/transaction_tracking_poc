2abdcfb9928e20e748be70ceda2e5b59b7cc007b
David Van Der Beek <earlynovrock@gmail.com>
2025-04-30 08:28:47 -0700

    Update transaction_tracking.rb

--- packs/features/transaction_tracking/transaction_tracking.rb
+++ packs/features/transaction_tracking/transaction_tracking.rb
@@ -3,6 +3,7 @@ require_relative "tracker"
 module TransactionTracking
   extend self
 
+  # This represents the public API for TransactionTracking
   def track(protocol:, tx_hash:, &block)
     Tracker.new(protocol: protocol).track(tx_hash, &block)
   end
